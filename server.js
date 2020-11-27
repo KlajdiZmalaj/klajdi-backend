ec428498689c0cfdeb1cc257d6c390eab936bb38
Klajdi <klajdizmalaj@icloud.com>
2020-11-27 16:17:24 +0100

    m

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.use(require("body-parser").json());
 //routes
 app.set("port", process.env.PORT || 5000);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 app.use("/api", require("./routes/projects"));
 
 //db connect
